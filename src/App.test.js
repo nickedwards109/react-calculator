5b796cccf2694bead8171189a440aee10653d0e2
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-09-03 14:18:18 -0600

    Test that a new component renders without crashing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import NumberContainer from './NumberContainer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders the NumberContainer component without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<NumberContainer />, div);
 });
---
+++ src/NumberContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class NumberContainer extends React.Component {
+	render() {
+		return <h1></h1>
+	}
+}
+
+export default NumberContainer;
