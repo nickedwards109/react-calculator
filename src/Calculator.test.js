b865895dde68e9f9443dfe77b68a0f92f7d6a507
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-09-05 13:04:24 -0600

    Add unit test for subtracting numbers

--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -9,4 +9,13 @@ describe('unit testing a calculator', () => {
 		const bigSum = calculator.add(10, 25, 1000, 2, 300);
 		expect(bigSum).toEqual(1337);
   });
+
+	it('subtracts numbers', () => {
+		const calculator = new Calculator();
+		const smallDifference = calculator.subtract(43, 1);
+		expect(smallDifference).toEqual(42);
+
+		const bigDifference = calculator.subtract(1000, 900, 90, 9);
+		expect(bigDifference).toEqual(1);
+	});
 });
