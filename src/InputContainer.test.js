2218e488a2206d06964851db100b205b83133042
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-09-07 22:17:36 -0600

    Add test for an input sanitization function
    Whenever the form is updated, a function should be called that will sanitize the input data into a number regardless of the input data type. If a number is entered into the form, that number should be returned from the sanitization function. If the data cannot be parsed into a number (such as a string or an empty input), the sanitization function should return 0.

--- src/InputContainer.test.js
+++ src/InputContainer.test.js
@@ -14,6 +14,21 @@ describe('form input component', () => {
 		expect(spy.calledWith(42)).toEqual(true);
 	});
 
+	it('calls getInputAsNumber() and returns a number when the form is updated', () => {
+		const spy = sinon.spy();
+		const inputContainer = shallow(<InputContainer handleInputChange={spy} />);
+		expect(spy.notCalled).toEqual(true);
+
+		inputContainer.find('input').simulate('change', {target: {value: '42'}});
+		expect(spy.calledWith(42)).toEqual(true);
+
+		inputContainer.find('input').simulate('change', {target: {value: 'asdf'}});
+		expect(spy.calledWith(0)).toEqual(true);
+
+		inputContainer.find('input').simulate('change', {target: {value: ''}});
+		expect(spy.calledWith(0)).toEqual(true);
+	});
+
 	it('renders an error message when string data is input', () => {
 		const inputContainer = shallow(<InputContainer />);
 		inputContainer.find('input').simulate('change', {target: {value: 'asdf'}});
